d7bdc46d07a96b5b4b91f2c8fab14cf870c3264f
dionysus777-333 <55891269+dionysus777-333@users.noreply.github.com>
2026-05-11 12:54:53 -0700

    tweaked purlexia user boilerplate

--- modules/hosts/purlexia/configuration.nix
+++ modules/hosts/purlexia/configuration.nix
@@ -12,20 +12,6 @@
       popups = lib.mkForce 14;
       desktop = lib.mkForce 12;
     };
-    home-manager.users.user = {
-      wayland.windowManager.hyprland.settings = {
-        device = [
-          {
-            name = "synaptics-tm2722-001";
-            accel_profile = "adaptive";
-          }
-          {
-            name = "tpps/2-ibm-trackpoint";
-            accel_profile = "adaptive";
-          }
-        ];
-      };
-    };
   };
 }
 
--- modules/hosts/purlexia/users/user.nix
+++ modules/hosts/purlexia/users/user.nix
@@ -3,12 +3,24 @@
   ...
 }:
 {
-  flake.modules.nixos.purlexia =
-    { config, ... }:
-    {
-      imports = [
-        self.modules.nixos.user 
-      ];
+  flake.modules.nixos.purlexia = { config, ... }: {
+    imports = [
+      self.modules.nixos.user 
+    ];
+    home-manager.users.user = {
+      wayland.windowManager.hyprland.settings = {
+        device = [
+          {
+            name = "synaptics-tm2722-001";
+            accel_profile = "adaptive";
+          }
+          {
+            name = "tpps/2-ibm-trackpoint";
+            accel_profile = "adaptive";
+          }
+        ];
+      };
     };
+  };
 }
 
