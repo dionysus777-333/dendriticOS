6a864b04535c06eb5a04ce89ba9512eb7cc1e5f4
dionysus777-333 <55891269+dionysus777-333@users.noreply.github.com>
2026-09-19 03:15:32 -0700

    driver changes for purlexia hardware config

--- modules/hosts/purlexia/_hardware-configuration.nix
+++ modules/hosts/purlexia/_hardware-configuration.nix
@@ -5,7 +5,7 @@
 
 {
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
