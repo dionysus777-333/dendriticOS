8666b84dc9625c827a7fb614c4cb55f093bfa244
dionysus777-333 <55891269+dionysus777-333@users.noreply.github.com>
2026-09-19 02:39:25 -0700

    commented out exception electrumltc uses

--- modules/templates/extras.nix
+++ modules/templates/extras.nix
@@ -5,7 +5,7 @@
       flatpak
     ];
     nixpkgs.config.permittedInsecurePackages = [
-      "python3.14-ecdsa-0.19.2"
+      #"python3.14-ecdsa-0.19.2"
     ];
     environment.systemPackages = with pkgs; [
       feishin
