0eaae8a3f9490d9d955b32461f42fbf433108bbb
dionysus777-333 <55891269+dionysus777-333@users.noreply.github.com>
2026-09-20 22:02:23 -0700

    added intel microcode to purlexia

--- modules/programs/electrum-ltc.nix
+++ modules/programs/electrum-ltc.nix
@@ -2,7 +2,7 @@
 {
   flake.modules.nixos.electrum-ltc = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
-      electrum-ltc
+      #electrum-ltc
     ];
     nixpkgs.config.permittedInsecurePackages = [
       "python3.14-ecdsa-0.19.2"
